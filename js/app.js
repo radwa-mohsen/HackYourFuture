9a20a29963cf90b896eda5be75e09b7f46f83ef9
radwa-mohsen <38859200+radwa-mohsen@users.noreply.github.com>
2024-03-08 14:05:19 +0100

    phase 2

--- js/app.js
+++ js/app.js
@@ -1,6 +1,40 @@
+// constants
+
 const menuIcon = document.querySelector(".menu-icon");
 const slideMenu = document.querySelector(".nav-list");
 const closeIcon = document.querySelector(".close-icon");
+const headerContainer = document.querySelector(".header-container");
+const modalContent = document.getElementById("modal-content");
+const myModal = document.getElementById("myModal");
+const eventCards = document.querySelectorAll(".dives-section .card");
+const memberCards = document.querySelectorAll(".members-section .card");
+const scrollTopButton = document.getElementById("scrollTopBtn");
+
+// functions
+
+const openModal = (content) => {
+  modalContent.innerHTML = content;
+  myModal.style.display = "block";
+};
+
+const closeModal = () => {
+  myModal.style.display = "none";
+};
+
+const scrollToTop = () => {
+  const scrollStep = -window.scrollY / (500 / 15);
+
+  function scroll() {
+    if (window.scrollY !== 0) {
+      window.scrollBy(0, scrollStep);
+      requestAnimationFrame(scroll);
+    }
+  }
+
+  requestAnimationFrame(scroll);
+};
+
+//listeners
 
 menuIcon.addEventListener("click", function () {
   slideMenu.classList.toggle("active");
@@ -10,40 +44,38 @@ closeIcon.addEventListener("click", function () {
   slideMenu.classList.toggle("active");
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  var headerContainer = document.querySelector(".header-container");
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 0) {
+    headerContainer.classList.add("scrolled");
+  } else {
+    headerContainer.classList.remove("scrolled");
+  }
+});
 
-  window.addEventListener("scroll", function () {
-    if (window.scrollY > 0) {
-      headerContainer.classList.add("scrolled");
-    } else {
-      headerContainer.classList.remove("scrolled");
-    }
+const getEventCardContent = (index) =>
+  document.getElementById(`card-event${index}`).innerHTML;
+
+const getMemberCardContent = (index) =>
+  document.getElementById(`card-member${index}`).innerHTML;
+
+eventCards.forEach((card, index) => {
+  card.addEventListener("click", () => {
+    const cardContent = getEventCardContent(index + 1);
+    openModal(cardContent);
   });
 });
 
-const slider = document.querySelector(".card-container");
-const slides = document.querySelectorAll(".card");
-const leftArrow = document.querySelector(".arrow-left");
-const rightArrow = document.querySelector(".arrow-right");
-
-let currentSlideIndex = 0;
-
-function moveSlide(index) {
-  debugger;
-  slider.style.transform = `translateX(-${index * 100}%)`;  
-  currentSlideIndex = index;
-}
-
-leftArrow.addEventListener("click", () => {
-  debugger;
-  const newIndex =
-    currentSlideIndex === 0 ? slides.length - 1 : currentSlideIndex - 1;
-  moveSlide(newIndex);
+memberCards.forEach((card, index) => {
+  card.addEventListener("click", () => {
+    const cardContent = getMemberCardContent(index + 1);
+    openModal(cardContent);
+  });
 });
 
-rightArrow.addEventListener("click", () => {
-  const newIndex =
-    currentSlideIndex === slides.length - 1 ? 0 : currentSlideIndex + 1;
-  moveSlide(newIndex);
-});
+window.onscroll = function () {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollTopButton.style.display = "flex";
+  } else {
+    scrollTopButton.style.display = "none";
+  }
+};
